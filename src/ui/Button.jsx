8ff2c80e65f34f1f30f570dbeb1edb5c58a7e30e
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-02-16 21:46:28 +0900

    add cart functions add delete update items in cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 /* eslint-disable react/prop-types */
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 ring-yellow-300 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "inline-block text-sm rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-400 ring-yellow-300 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5",
   };
@@ -17,6 +18,13 @@ function Button({ children, disabled, to, type }) {
       <Link className={styles[type]} to={to}>
         {children}
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
 
   return (
