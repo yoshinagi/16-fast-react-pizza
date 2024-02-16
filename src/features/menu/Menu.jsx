516c3b416dcf99936e3dc185cc5f814e227fef42
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-02-16 10:47:35 +0900

    add tailwind css

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData } from 'react-router-dom';
-import { getMenu } from '../../services/apiRestaurant';
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
 
-import MenuItem from './MenuItem';
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
